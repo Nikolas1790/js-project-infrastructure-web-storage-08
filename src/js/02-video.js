3db026ee6ac4d3978845e255b5cd30c4c7863e07
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-06-30 19:21:54 +0300

    more-better

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,39 +4,19 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 
-const options = {
-        id: 59777392,
-        width: 640,
-        loop: true
-    };
-const player = new Player(iframe, options);
+
+const player = new Player(iframe);
 
 
-const onPlay = function (currentTime) {
-     // data is an object containing properties specific to that event
+const onPlay = function (currentTime) {    
     const currentSec = currentTime.seconds;
    
     localStorage.setItem("videoplayer-current-time", JSON.stringify(currentSec));
-    console.log(localStorage.getItem("videoplayer-current-time"))   
-};
+    };
 
 player.on("timeupdate", throttle(onPlay, 1000));
 
-player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time"))).then(function(seconds) {
-    // seconds = JSON.parse(localStorage.g etItem("videoplayer-current-time"))
-    
-
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
+player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")))
 
 
 
